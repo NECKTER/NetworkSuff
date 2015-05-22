b7e8c854a6f33397300e7f6798df5d5582351461
Mcranky <mark@kawaii-potato.amadorpusd.pleasanton.k12.ca.us>
2015-05-22 12:40:28 -0700

    update

--- src/Networkstuuff/GameClient.java
+++ src/Networkstuuff/GameClient.java
@@ -1,53 +1,52 @@
 package Networkstuuff;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 import Mainpackage.Panel;
-import NetworkSuff;
 
-public class GameClient extends Thread {
-private InetAddress ipAddress;
-private DatagramSocket socket;
-private Panel panel;
-public GameClient(Panel pan,String ipAdress){
-	this.panel=pan;
-	try {
-		this.socket=new DatagramSocket();
-		this.ipAddress=InetAddress.getByName(ipAdress);
-	} catch (SocketException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (UnknownHostException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+public class GameClient {
+	public static int[] clientsvalues=new int[9];
+	Panel l;
 	
-	
-}
-public void run(){
-	while(true){
-		byte[]data =new byte[1024];//can change size of package 
-		DatagramPacket packet = new DatagramPacket(data,data.length);
+	public static void main(String[] args){
+		String info;
+		
+		 Socket skt;
 		try {
-			socket.receive(packet);
+			
+			skt = new Socket("localhost", 1234);
+			BufferedReader in = new BufferedReader(new
+		            InputStreamReader(skt.getInputStream()));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(skt.getOutputStream()));//, true);
+			
+		         System.out.print("Received string: '");
+                 
+		         while (!in.ready()) {}
+		        // Read one line and output it
+                for(int i=0;i<9;i++){
+                	clientsvalues[i]=in.readLine().trim().charAt(i); 
+                }
+		         
+		         in.close();
+		         
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Server>"+new String(packet.getData()));
+		
+         
+	
+	
 	}
+	
 }
-public void sendData(byte[]data){
-	DatagramPacket packet=new DatagramPacket(data,data.length,ipAddress,1331);//change port, above 1024
-	try {
-		socket.send(packet);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-}
-}
+
--- src/Networkstuuff/GameServer.java
+++ src/Networkstuuff/GameServer.java
@@ -1,5 +1,54 @@
 package Networkstuuff;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import Mainpackage.Panel;
 public class GameServer {
 
+	public GameServer() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public static void main(String[] args){
+		int [] states=new int [8];
+		String data="Swag city";
+		Boolean hj;
+		while(true){
+		try {
+			ServerSocket srvr=new ServerSocket(1234);
+			Socket skt=srvr.accept();
+			System.out.println("It connected");
+			BufferedReader in = new BufferedReader(
+		            new InputStreamReader (skt.getInputStream()));
+			while (!in.ready()) {}
+			
+			for(int i =0;i<8;i++){
+				states[i]=in.readLine().trim().charAt(i);
+			}
+			PrintWriter out = new PrintWriter(skt.getOutputStream(),true);	
+			for (int l=0;l<8;l++){
+			 data=data+states[l];
+			}
+			out.write(data);;
+	        
+			in.close();
+			out.close();
+			skt.close();
+			srvr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Didnt work");
+		}
+	}
+	}
+
 }
+
